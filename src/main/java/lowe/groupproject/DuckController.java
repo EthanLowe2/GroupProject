a39b64f78f5d7cd603ab2f3067e86c6b9bffff76
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-12-09 14:30:04 -0500

    Duck adding

---
+++ src/main/java/lowe/groupproject/DuckController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lowe.groupproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ethanl82
+ */
+public class DuckController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
