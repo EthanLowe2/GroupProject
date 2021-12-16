586203d8425bfa1ff8bf558202c31ebe5d31b847
ethanl82 <ethanl82@BHSCUR149020SPW.kprdsb.schools.local>
2021-12-16 14:19:10 -0500

    Bonus Ducks

--- src/main/java/lowe/groupproject/DuckController.java
+++ src/main/java/lowe/groupproject/DuckController.java
@@ -14,6 +14,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 import javafx.util.Duration;
 
@@ -33,9 +34,12 @@ public class DuckController implements Initializable {
 
     @FXML
     private Line wallRight, wallDown, wallLeft, wallUp;
-    
+
     @FXML
-    private ImageView imgDuck1,imgDuck2,imgDuck3,imgDuck4,imgDuck5,imgDuck6;
+    private ImageView imgDuck1, imgDuck2, imgDuck3, imgDuck4, imgDuck5, imgDuck6;
+
+    @FXML
+    private Polygon PDuck0, PDuck1, PDuck2, PDuck3, PDuck4, PDuck5;
 
     Timeline clock = new Timeline(new KeyFrame(Duration.seconds(1), ae -> timer()));
     Timeline count = new Timeline(new KeyFrame(Duration.seconds(1), ae -> CountDown()));
@@ -90,27 +94,27 @@ public class DuckController implements Initializable {
     void pickDuck() {
         Duck = ThreadLocalRandom.current().nextInt(1, 6 + 1);
         if (Duck == 1 && Duck1.getTranslateX() == 0) {
-            ducks[0] = new duckMove(Duck1, false, wallDown, wallLeft, wallRight, wallUp,imgDuck1);
+            ducks[0] = new duckMove(Duck1, false, wallDown, wallLeft, wallRight, wallUp, imgDuck1, PDuck0, 0);
             ducks[0].start();
         }
         if (Duck == 2 && Duck2.getTranslateX() == 0) {
-            ducks[1] = new duckMove(Duck2, false, wallDown, wallLeft, wallRight, wallUp,imgDuck2);
+            ducks[1] = new duckMove(Duck2, false, wallDown, wallLeft, wallRight, wallUp, imgDuck2, PDuck1, 1);
             ducks[1].start();
         }
         if (Duck == 3 && Duck3.getTranslateX() == 0) {
-            ducks[2] = new duckMove(Duck3, false, wallDown, wallLeft, wallRight, wallUp,imgDuck3);
+            ducks[2] = new duckMove(Duck3, false, wallDown, wallLeft, wallRight, wallUp, imgDuck3, PDuck2, 2);
             ducks[2].start();
         }
         if (Duck == 4 && Duck4.getTranslateX() == 0) {
-            ducks[3] = new duckMove(Duck4, true, wallDown, wallLeft, wallRight, wallUp,imgDuck4);
+            ducks[3] = new duckMove(Duck4, true, wallDown, wallLeft, wallRight, wallUp, imgDuck4, PDuck3, 3);
             ducks[3].start();
         }
         if (Duck == 5 && Duck5.getTranslateX() == 0) {
-            ducks[4] = new duckMove(Duck5, true, wallDown, wallLeft, wallRight, wallUp,imgDuck5);
+            ducks[4] = new duckMove(Duck5, true, wallDown, wallLeft, wallRight, wallUp, imgDuck5, PDuck4, 4);
             ducks[4].start();
         }
         if (Duck == 6 && Duck6.getTranslateX() == 0) {
-            ducks[5] = new duckMove(Duck6, true, wallDown, wallLeft, wallRight, wallUp,imgDuck6);
+            ducks[5] = new duckMove(Duck6, true, wallDown, wallLeft, wallRight, wallUp, imgDuck6, PDuck5, 5);
             ducks[5].start();
         }
     }
@@ -118,41 +122,33 @@ public class DuckController implements Initializable {
     @FXML
     void OnClick(MouseEvent event) {
         Shape poly = (Shape) event.getSource();
-        int spot = Integer.parseInt(""+poly.getAccessibleText().charAt(0));
-        System.out.println(spot);
-        /* for (int i = 0; i < ducks.length; i++) {
-        if (ducks[i].inUse && poly.equals(ducks[i].polDock)) {
-        spot = i;
-        }
-        }*/
-        
         if (poly.getAccessibleText().equals("B")) {
             Reload();
             ShowBull();
-
+            return;
         }
-    //    if (Running == true) {
-      
-            if(Bullets != 0 && ducks[spot].inUse){
-            if ((""+poly.getAccessibleText().charAt(1)).equals("g")) {
+        //    if (Running == true) {
+        int spot = Integer.parseInt("" + poly.getAccessibleText().charAt(0));
+        if (Bullets != 0 && ducks[spot].inUse) {
+            if (("" + poly.getAccessibleText().charAt(1)).equals("g")) {
                 points = points + 5;
                 Bullets--;
                 ShowBull();
                 ducks[spot].shoot();
-            } else if (poly.getAccessibleText().equals("y")) {
+            } else if (("" + poly.getAccessibleText().charAt(1)).equals("y")) {
                 points = points + 3;
                 Bullets--;
                 ShowBull();
                 ducks[spot].shoot();
-            } else if (poly.getAccessibleText().equals("r")) {
+            } else if (("" + poly.getAccessibleText().charAt(1)).equals("r")) {
                 points = points + 1;
                 Bullets--;
                 ShowBull();
                 ducks[spot].shoot();
             }
-            }
         }
-   // }
+    }
+    // }
 
     void Points(int value) {
         int oldpoints;
--- src/main/java/lowe/groupproject/duckMove.java
+++ src/main/java/lowe/groupproject/duckMove.java
@@ -24,6 +24,9 @@ public class duckMove {
     private Line wall2;
     private Line wall3;
     private Line wall4;
+    private int DuckCol;
+    private Polygon PolyDuck;
+    private boolean side;
     public boolean inUse = false;
     Timeline duckmove = new Timeline(new KeyFrame(Duration.millis(50), ae -> moveDuck()));
 
@@ -31,7 +34,7 @@ public class duckMove {
 
     }
 
-    public duckMove(Pane movePane, boolean side, Line Twall1, Line Twall2, Line Twall3, Line Twall4, ImageView TimgDuck) {
+    public duckMove(Pane movePane, boolean Side, Line Twall1, Line Twall2, Line Twall3, Line Twall4, ImageView TimgDuck, Polygon TempPoly, int DuckNum) {
         int DuckX = ThreadLocalRandom.current().nextInt(1, 12 + 1);
         int DuckY = ThreadLocalRandom.current().nextInt(-8, -2 + 1);
         x = DuckX;
@@ -43,19 +46,48 @@ public class duckMove {
         wall2 = Twall2;
         wall3 = Twall3;
         wall4 = Twall4;
+        side = Side;
+        PolyDuck = TempPoly;
         inUse = true;
         if (side == true) {
             x = -x;
             imgDuck.setImage(new Image(getClass().getResource("/DuckHuntL.png").toString()));
-        }else{
+        } else {
             imgDuck.setImage(new Image(getClass().getResource("/GreenDuck.png").toString()));
         }
         pnlMove = movePane;
-          
+        DuckCol = ThreadLocalRandom.current().nextInt(1, 50 + 1);
+        if (DuckCol <= 50 && DuckCol > 40) {
+            PolyDuck.setAccessibleText(DuckNum + "r");
+            if (side == true) {
+                imgDuck.setImage(new Image(getClass().getResource("/DuckRL.png").toString()));
+            }
+            if (side == false) {
+                imgDuck.setImage(new Image(getClass().getResource("/DuckR.png").toString()));
+            }
+        }
+        if (DuckCol <= 40 && DuckCol > 25) {
+            PolyDuck.setAccessibleText(DuckNum + "y");
+            if (side == true) {
+                imgDuck.setImage(new Image(getClass().getResource("/DuckPL.png").toString()));
+            }
+            if (side == false) {
+                imgDuck.setImage(new Image(getClass().getResource("/DuckP.png").toString()));
+            }
+        }
+        if (DuckCol <= 25 && DuckCol >= 0) {
+            PolyDuck.setAccessibleText(DuckNum + "g");
+        }
     }
 
     private void moveDuck() {
-        if (y==3){
+        if (y == 6 && DuckCol <= 50 && DuckCol > 40) {
+            imgDuck.setImage(new Image(getClass().getResource("/DuckRFall.png").toString()));
+        }
+        if (y == 6 && DuckCol <= 40 && DuckCol > 25) {
+            imgDuck.setImage(new Image(getClass().getResource("/DuckPFall.png").toString()));
+        }
+        if (y == 6 && DuckCol <= 25 && DuckCol >= 0) {
             imgDuck.setImage(new Image(getClass().getResource("/DuckFall.png").toString()));
         }
         pnlMove.setTranslateX(pnlMove.getTranslateX() + x);
@@ -66,6 +98,7 @@ public class duckMove {
             x = 0;
             y = 0;
             inUse = false;
+            duckmove.stop();
         }
         if (collision(pnlMove, wall2)) {
             pnlMove.setTranslateX(0);
@@ -73,6 +106,7 @@ public class duckMove {
             x = 0;
             y = 0;
             inUse = false;
+            duckmove.stop();
         }
         if (collision(pnlMove, wall3)) {
             pnlMove.setTranslateX(0);
@@ -80,6 +114,7 @@ public class duckMove {
             x = 0;
             y = 0;
             inUse = false;
+            duckmove.stop();
         }
         if (collision(pnlMove, wall4)) {
             pnlMove.setTranslateX(0);
@@ -87,6 +122,7 @@ public class duckMove {
             x = 0;
             y = 0;
             inUse = false;
+            duckmove.stop();
         }
     }
 
@@ -98,10 +134,18 @@ public class duckMove {
         inUse = false;
         x = 0;
         y = 0;
-        imgDuck.setImage(new Image(getClass().getResource("/DuckShot.png").toString()));
+        if (DuckCol <= 50 && DuckCol > 40) {
+            imgDuck.setImage(new Image(getClass().getResource("/DuckRShot.png").toString()));
+        }
+        if (DuckCol <= 40 && DuckCol > 25) {
+            imgDuck.setImage(new Image(getClass().getResource("/DuckPShot.png").toString()));
+        }
+        if (DuckCol <= 25 && DuckCol >= 0) {
+            imgDuck.setImage(new Image(getClass().getResource("/DuckShot.png").toString()));
+        }
         duckmove.stop();
         duckmove.setDelay(Duration.seconds(1));
-        y=3;
+        y = 6;
         duckmove.play();
     }
 
